56905b6211340572b78ab0b453e4265da0bd3a83
KenEvans <kenev007@gmail.com>
2021-04-17 15:17:55 -0700

    modified to support new schema

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var router = express.Router();
 // .ADO.Net is a wrapper over raw SQL server interface
 const mongoose = require("mongoose");
 
-const ToDos = require("../ToDos");
+const Hangers = require("../hangers");
 
 // edited to include my non-admin, user level account and PW on mongo atlas
 // and also to include the name of the mongo DB that the collection
 const dbURI =
- "your mongo connection string here";
+ "mongodb+srv://bcuser:bcuser@cluster0.spomk.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
 // by default, you need to set it to false.
@@ -37,10 +37,12 @@ router.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
+
+
 /* GET all ToDos */
 router.get('/ToDos', function(req, res) {
   // find {  takes values, but leaving it blank gets all}
-  ToDos.find({}, (err, AllToDos) => {
+  Hangers.find({}, (err, AllToDos) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
@@ -51,11 +53,10 @@ router.get('/ToDos', function(req, res) {
 
 
 
-
 /* post a new ToDo and push to Mongo */
 router.post('/NewToDo', function(req, res) {
 
-    let oneNewToDo = new ToDos(req.body);  // call constuctor in ToDos code that makes a new mongo ToDo object
+    let oneNewToDo = new Hangers(req.body);  // call constuctor in ToDos code that makes a new mongo ToDo object
     console.log(req.body);
     oneNewToDo.save((err, todo) => {
       if (err) {
@@ -69,39 +70,49 @@ router.post('/NewToDo', function(req, res) {
 });
 
 
-router.delete('/DeleteToDo/:id', function (req, res) {
-  ToDos.deleteOne({ _id: req.params.id }, (err, note) => { 
+
+/* delete an existing Hanger record from Mongo DB */
+router.delete('/DeleteHanger/:id', function (req, res) {
+  Hangers.deleteOne({ _id: req.params.id }, (err, note) => { 
     if (err) {
       res.status(404).send(err);
     }
-    res.status(200).json({ message: "ToDo successfully deleted" });
+    res.status(200).json({ message: "Hanger successfully deleted" });
   });
 });
 
 
-router.put('/UpdateToDo/:id', function (req, res) {
-  ToDos.findOneAndUpdate(
+
+/* put (update) one Hanger */
+router.put('/UpdateHanger/:id', function (req, res) {
+  Hangers.findOneAndUpdate(
     { _id: req.params.id },
-    { title: req.body.title, detail: req.body.detail, priority: req.body.priority,   completed: req.body.completed },
+    { hangerName: req.body.hangerName, 
+      construction: req.body.construction, 
+      color: req.body.color, 
+      sturdiness: req.body.sturdiness,
+      pantClips: req.body.pantClips
+    },
    { new: true },
-    (err, todo) => {
+    (err, updHanger) => {
       if (err) {
         res.status(500).send(err);
     }
-    res.status(200).json(todo);
+    res.status(200).json(updHanger);
     })
   });
 
 
-  /* GET one ToDos */
-router.get('/FindToDo/:id', function(req, res) {
+
+  /* get one Hangers */
+router.get('/FindHanger/:id', function(req, res) {
   console.log(req.params.id );
-  ToDos.find({ _id: req.params.id }, (err, oneToDo) => {
+  Hangers.find({ _id: req.params.id }, (err, oneHanger) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
     }
-    res.status(200).json(oneToDo);
+    res.status(200).json(oneHanger);
   });
 });
 
